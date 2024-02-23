efbc82ecfccc28dce9d60083997a3d8ca3487f47
SmeysyaHromche <MyrKykhta@gmail.com>
2024-02-23 12:54:46 +0100

    add tools part for app with icons

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -2,16 +2,18 @@
 #include "ui_mainwindow.h"
 #include <QPushButton>
 #include <iostream>
+
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
     QApplication::setApplicationDisplayName("ICP2024");
-    initAppWindows();
-    runApp();
+    createAppWindows();
     //centralWidget->setStyleSheet("QWidget { background-color: white; }");
 }
+
 
 MainWindow::~MainWindow()
 {
@@ -19,31 +21,85 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::runApp(){
-    menuWind->show();
-    //simWind->show();
+
+void MainWindow::createAppWindows(){
+    createActions();
+    createTools();
 }
 
-void MainWindow::initAppWindows(){
-    menuWind = new MenuWindow();
-    simWind = new SimulationWindow();
-    connect(menuWind->startButton, SIGNAL(clicked()), this, SLOT(fromMenuToSimulation()));
-    connect(simWind->startButton, SIGNAL(clicked()), this, SLOT(fromSimulationToMenu()));
-}
 
 void MainWindow::deleteAppWindows(){
-    delete menuWind;
-    delete simWind;
+    deleteActions();
+    deleteTools();
 }
 
-void MainWindow::fromMenuToSimulation(){
-    menuWind->hide();
-    simWind->show();
+
+void MainWindow::createActions(){
+    helpToolAction = new QAction(QIcon(":/icons/helpTool.jpg"), "&Help", this);
+    connect(helpToolAction, SIGNAL(triggered()), this, SLOT(helpTextToolActionFunctional()));
+
+    newMapToolAction = new QAction(QIcon(":/icons/newMapTool.png"), "&New map", this);
+    connect(newMapToolAction, SIGNAL(triggered()), this, SLOT(newMapToolActionFunctional()));
+
+    listMapToolAction = new QAction(QIcon(":/icons/listOfMapTool.png"), "&List of maps", this);
+    connect(listMapToolAction, SIGNAL(triggered()), this, SLOT(listMapToolActionFunctional()));
+
+    settingsToolAction = new QAction(QIcon(":/icons/settingsTool.png"), "&Settings", this);
+    connect(settingsToolAction, SIGNAL(triggered()), this, SLOT(settingsToolActionFunctional()));
 }
 
-void MainWindow::fromSimulationToMenu(){
-    simWind->hide();
-    menuWind->show();
+
+void MainWindow::deleteActions(){
+    disconnect(helpToolAction, 0, 0, 0);
+    disconnect(newMapToolAction, 0, 0, 0);
+    disconnect(listMapToolAction, 0, 0, 0);
+    disconnect(settingsToolAction, 0, 0, 0);
+
+    delete helpToolAction;
+    delete newMapToolAction;
+    delete listMapToolAction;
+    delete settingsToolAction;
+}
+
+
+void MainWindow::createTools(){
+    helpToolBar = addToolBar("&help");
+    helpToolBar->addAction(helpToolAction);
+
+    mapToolBar = addToolBar("&maps");
+    mapToolBar->addAction(newMapToolAction);
+    mapToolBar->addAction(listMapToolAction);
+
+    settingsToolBar = addToolBar("&settings");
+    settingsToolBar->addAction(settingsToolAction);
+}
+
+
+void MainWindow::deleteTools(){
+    delete helpToolBar;
+    delete mapToolBar;
+    delete settingsToolBar;
+}
+
+
+void MainWindow::helpTextToolActionFunctional(){
+    // !!! need add help text for users interface actions
+    QMessageBox::about(this, "Help", "<b>This place will be for help users text!</b>");
+}
+
+void MainWindow::newMapToolActionFunctional(){
+    // !!! creatting widget for writting new map, parsing and sending do simulations widget creatting
+    QMessageBox::about(this, "Help", "<b>This place will be for creatting new maps!</b>");
+}
+
+void MainWindow::listMapToolActionFunctional(){
+    // !!! creatting widget list all existting maps and choosing one actual or delete
+    QMessageBox::about(this, "Help", "<b>This place will be for creatting new maps!</b>");
+}
+
+void MainWindow::settingsToolActionFunctional(){
+    // !!! creatting widget for setting
+    QMessageBox::about(this, "Help", "<b>This place will be for setting!</b>");
 }
 
 
--- src/gui/mainwindow.h
+++ src/gui/mainwindow.h
@@ -6,15 +6,15 @@
 #include <QPushButton>
 #include <QLabel>
 #include <QVBoxLayout>
-
-#include "simulationwindow.h"
-#include "menuwindow.h"
-
-
+#include <QToolBar>
+#include <QToolButton>
+#include <QAction>
+#include <iostream>
+#include <QIcon>
+#include <QMessageBox>
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
-class SimulationWindow;
 }
 QT_END_NAMESPACE
 
@@ -28,18 +28,69 @@ public:
 
 private:
 
-    MenuWindow* menuWind;
-    SimulationWindow* simWind;
-    void initAppWindows();
-    void deleteAppWindows();
-    void runApp();
-    QPushButton* testButton;
-    //void runApplication();
+    // BAR && ACTIONS
 
+    QToolBar* helpToolBar;
+    QAction* helpToolAction;
+
+    QToolBar* mapToolBar;
+    QAction* newMapToolAction;
+    QAction* listMapToolAction;
+
+    QToolBar* settingsToolBar;
+    QAction* settingsToolAction;
+
+    // UI
     Ui::MainWindow *ui;
 
+
+    // METHODS
+    /**
+     * @brief creatting default body of applications window
+     * @param void
+     * @return void
+    */
+    void createAppWindows();
+
+    /**
+     * @brief deleting all defaults widget of applications widnows body
+     * @param void
+     * @return void
+    */
+    void deleteAppWindows();
+
+    /**
+     * @brief creating all applications windows tools and adding their actions
+     * @param void
+     * @return void
+    */
+    void createTools();
+
+    /**
+     * @brief creating all tools actions and connect their with functional slots
+     * @param void
+     * @return void
+    */
+    void createActions();
+
+    /**
+     * @brief clean tools memmory
+     * @param void
+     * @return void
+    */
+    void deleteTools();
+
+    /**
+     * @brief disconnect actions with slots and clean their memmory
+     * @param void
+     * @return void
+    */
+    void deleteActions();
+
 public slots:
-    void fromMenuToSimulation();
-    void fromSimulationToMenu();
+    void helpTextToolActionFunctional();
+    void newMapToolActionFunctional();
+    void listMapToolActionFunctional();
+    void settingsToolActionFunctional();
 };
 #endif // MAINWINDOW_H
