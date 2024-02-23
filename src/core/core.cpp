9a04757ed7cf9469694283c0702c4d2e03f138ea
hiraethese <baturovilya123@gmail.com>
2024-02-23 23:45:00 +0100

    Minor changes

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -1,9 +1,9 @@
 #include "core.h"
-#include "entities/SimObject.h"
+
+SimObject* newRect = nullptr;
 
 int SimStart()
 {
-    SimObject* newRect;
-    newRect = new SimObject( {400.0f, 400.0f}, {20.0f, 20.0f} );
+    newRect = new SimObject({400.0f, 400.0f}, {20.0f, 20.0f});
     return 0;
 }
--- src/core/core.h
+++ src/core/core.h
@@ -1,6 +1,10 @@
 #ifndef CORE_H
 #define CORE_H
 
+#include "entities/SimObject.h"
+
+extern SimObject* newRect;  
+
 int SimStart();
 
 #endif // CORE_H
