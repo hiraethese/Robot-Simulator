2adf4c6f281c567506e21d4e4d78bc06d1eab6cd
hiraethese <baturovilya123@gmail.com>
2024-03-17 15:49:51 +0100

    Delete all objects implementation

--- src/core/SimFactory.cpp
+++ src/core/SimFactory.cpp
@@ -5,6 +5,24 @@ SimFactory::SimFactory()
     _controlledRobot = new Robot({50.0f, 50.0f}, {100.0f, 100.0f}, 5.0f, 45, 0);
 }
 
+void SimFactory::DeleteAllObjects()
+{
+    if (_controlledRobot != nullptr)
+    {
+        delete _controlledRobot;
+        _controlledRobot = nullptr;
+    }
+
+    if (!_walls.empty())
+    {
+        for (auto& _wall : _walls)
+        {
+            delete _wall;
+        }
+        _walls.clear();
+    }
+}
+
 void SimFactory::AddWall(Wall* wall)
 {
     _walls.push_back(wall);
--- src/core/SimFactory.h
+++ src/core/SimFactory.h
@@ -13,6 +13,7 @@ private:
 
 public:
     SimFactory();
+    void DeleteAllObjects();
     void AddWall(Wall* wall);
     Robot* GetControlledRobot();
 };
