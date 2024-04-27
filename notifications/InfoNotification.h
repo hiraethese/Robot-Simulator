71799e64ad419067cc2e56a1f6ffa9a005bc5f86
SmeysyaHromche <MyrKykhta@gmail.com>
2024-04-27 14:18:23 +0200

    recreate notif

---
+++ notifications/InfoNotification.h
@@ -0,0 +1,13 @@
+#ifndef INFONOTIFICATION_H
+#define INFONOTIFICATION_H
+
+#include <QMessageBox>
+#include "../../core/icplib.h"
+
+class InfoNotification
+{
+protected:
+    void _WarningMsgSimNotSet(ICP_CODE code);
+};
+
+#endif // INFONOTIFICATION_H
